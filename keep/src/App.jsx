3de1707ad5658d756523db5906303192d7c24aea
juanpablo-git <salesjuan781@gmail.com>
2022-11-29 07:26:44 -0300

    .

--- keep/src/App.jsx
+++ keep/src/App.jsx
@@ -1,15 +1,24 @@
 import { useRef,useState} from 'react'
-import reactLogo from './assets/react.svg'
 import Modal from './modal'
 import Imput from './imput'
 import "./app.css"
 function App() {
   const [displayTitulo,setDisplayTitulo] = useState(0)
+  const referencia = useRef()
   return (
 <>
    
     <div className="App">
       <div   className='container-input'>
+        <textarea ref={referencia} onInput={(e)=>{
+           if (referencia.current.rows >= 1 && referencia.current.scrollHeight > referencia.current.offsetHeight) referencia.current.rows += 1
+
+           if (referencia.current.rows >= 1 && referencia.current.scrollHeight < referencia.current.offsetHeight) referencia.current.rows -= 1
+
+          console.log(referencia.current)
+        }} name="" id="" cols="30" rows="2">
+
+        </textarea>
       <Imput setDisplayTitulo={setDisplayTitulo} displayTitulo={displayTitulo} />
       </div>
       <div className='container-modal'>
--- keep/src/imput/index.jsx
+++ keep/src/imput/index.jsx
@@ -1,55 +1,59 @@
-import { useRef,useReducer } from "react"
+import { useRef, useReducer } from "react"
 import { focusTextArea } from "./events";
 import "./style.css";
-export default ({displayTitulo,setDisplayTitulo}) => {
+export default ({ displayTitulo, setDisplayTitulo }) => {
     const textArea = useRef()
-    const reducer = (state,action)=>{
-        switch(action.type){
+    const reducer = (state, action) => {
+        switch (action.type) {
             case "noEnvolto":
                 return {
                     displayEnvolto: !state.displayEnvolto
                 }
-           
-                
         }
     }
-    const [state,dicpach] = useReducer(reducer,{displayEnvolto:false})
-   
+    const [state, dicpach] = useReducer(reducer, { displayEnvolto: false })
+
     return (
         <>
-       
-       <div style={{position:"fixed",width:"100vw",height:"100vh",top:0,display:state.displayEnvolto ? "flex" : "none"}} onClick={()=>{
-        setDisplayTitulo(0)
-        dicpach({type:"noEnvolto"})
-        }} className="envolto">
-        </div>
-        
-        <div style={{zIndex:100}}   >
+
+            <div style={{ position: "fixed", width: "100vw", height: "100vh", top: 0, display: state.displayEnvolto ? "flex" : "none" }} onClick={() => {
+                setDisplayTitulo(0)
+                dicpach({ type: "noEnvolto" })
+            }} className="envolto">
+            </div>
+
+            <div style={{ zIndex: 100 }}   >
 
                 <div>
 
-                    <input placeholder="Titulo" style={{ display: displayTitulo ==1 ? "flex" : "none" }} />
+                    <input placeholder="Titulo" style={{ display: displayTitulo == 1 ? "flex" : "none" }} />
                 </div>
                 <div >
                     <textarea ref={textArea} autoFocus className="textArea"
-                    rows='1'  cols="70"
-                    onKeyDown={(e)=>{
-                        console.log(e.key)
-                        if(e.key == "Enter") textArea.current.rows +=1
-                        if(e.key == "Delete" && textArea.current.rows > 1 ) textArea.current.rows -=1
-                        if (textArea.current.value.length%69 == 0) textArea.current.rows +=1
-                    }}
-                    onClick={() =>{
-                        setDisplayTitulo(1)
-                        dicpach({type:"noEnvolto"})
+                        rows='1' cols="70"
+                        onKeyDown={(e) => {
+                            if (textArea.current.scrollHeight > textArea.current.offsetHeight){
+                                console.log(textArea.current.scrollHeight > textArea.current.offsetHeight)
+                                textArea.current.rows += 1
+                            } 
 
-                    }}   name="" id="" placeholder=" Crie uma nota...">
+                            if ( textArea.current.scrollHeight < textArea.current.offsetHeight){
+                                console.log(textArea.current.scrollHeight < textArea.current.offsetHeight)
+                                textArea.current.rows -= 1
+                            } 
+
+                        }}
+                        onClick={() => {
+                            setDisplayTitulo(1)
+                            dicpach({ type: "noEnvolto" })
+
+                        }} name="" id="" placeholder=" Crie uma nota...">
 
                     </textarea>
                 </div>
             </div>
-            </>
+        </>
 
 
-            )
+    )
 }
