a331197e61ce11da793159b7b99b62de79adc900
juanpablo-git <salesjuan781@gmail.com>
2022-11-30 20:47:46 -0300

    show npta

--- keep/src/nota/index.jsx
+++ keep/src/nota/index.jsx
@@ -2,7 +2,9 @@ export default ({
     titulo,nota
 })=>{
     return (
-    <div style={{display:"flex",justifyContent:"center",position:"fixed",top:0,left:0,backgroundColor:"GrayText",width:"100vw",height:"100vh",zIndex:"150"}}>
+    <div 
+    onClick={()=>{console.log("Fechou")}}
+    style={{display:"flex",justifyContent:"center",position:"fixed",top:0,left:0,backgroundColor:"GrayText",width:"100vw",height:"100vh",zIndex:"150"}}>
         <div style={{display:"flex",flexDirection:"column",width:"50%",alignItems:"center",backgroundColor:"InfoText"}}>
 
         <textarea style={{width:"90%"}} rows="1">{titulo}</textarea>
